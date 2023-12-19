65c9e7d7bd4e954a3b8c75d374d25c436d363db6
shirJoopy <shir.l@incentives-solutions.com>
2023-12-19 13:59:54 +0200

    export Wall and Message

--- src/componenets/Feed/Wall/Wall.tsx
+++ src/componenets/Feed/Wall/Wall.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 import Message, { DBMessageType, MessageType } from "../Message/Message"
+import { FC } from "react"
 
 
 const StyledWall = styled.div`
@@ -17,7 +18,9 @@ type WallProps = {
     messages:MessageType[]
 }
 
-export default ({messages}:WallProps)=>{
 
+const Wall :FC<WallProps>= ({messages})=>{
     return <StyledWall>{messages.map((message,index)=><Message {...message} index={index} />)}</StyledWall>
 }
+
+export default Wall;
