a9fe7a659813f948b946bc0baacabbad7139de3d
shirJoopy <shir.l@incentives-solutions.com>
2023-12-19 13:51:41 +0200

    version

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 
 
-export {default as JoopyTiles} from './componenets/Tiles';
+export {default as JoopyTiles} from './componenets/Tiles'
+
+
+export {default as Wall} from './componenets/Feed/Wall';
+
+export {default as Message} from './componenets/Feed/Message';
+
